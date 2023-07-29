ebf30b6d0ce48a62cd5949b87ffa53d69ca678ca
Abhinav Sinha <abhinavsinha135@gmail.com>
2023-07-29 23:33:37 +0530

    first commit

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 // We require the Hardhat Runtime Environment explicitly here. This is optional
 // but useful for running the script in a standalone fashion through `node <script>`.
 //
@@ -12,6 +13,32 @@ const tokens = (n) => {
 }
 
 async function main() {
+  // Setup Accounts
+  const [deployer] = await ethers.getSigners();
+
+  // Deploy Dappazon
+  const Dappazon = await hre.ethers.getContractFactory("Dappazon");
+  const dappazon = await Dappazon.deploy();
+  await dappazon.deployed();
+
+  console.log(`Deployed Dappazon Contract at : ${dappazon.address}\n`);
+
+  // Listing Items
+  for(let i = 0; i < items.length; i++) {
+    const transaction = await dappazon.connect(deployer).list(
+      items[i].id,
+      items[i].name,
+      items[i].category,
+      items[i].image,
+      tokens(items[i].price),
+      items[i].rating,
+      items[i].stock,
+    )
+
+    await transaction.wait();
+
+    console.log(`Listed item ${items[i].id}: ${items[i].name}`) 
+  }
 
 }
 
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,12 +1,52 @@
-import { ethers } from 'ethers';
+import { ethers } from "ethers";
 
 const Navigation = ({ account, setAccount }) => {
+    const connectHandler = async () => {
+      // window.ethereum.on("chainChanged", () => {
+      //   window.location.reload();
+      // });
 
-    return (
-        <nav>
+      // window.ethereum.on("accountsChanged", () => {
+      //   window.location.reload();
+      // });
+        const accounts = await window.ethereum.request({
+          method: "eth_requestAccounts",
+        });
+        const account = ethers.utils.getAddress(accounts[0]);
+        setAccount(account);
+    }
 
-        </nav>
-    );
-}
+  return (
+    <nav>
+      <div className="nav__brand">
+        <h1>Dappazon</h1>
+      </div>
+
+      <input type="text" className="nav__search" />
+
+      {account ? (
+        <button type="button" className="nav__connect">
+          {account.slice(0, 6) + "..." + account.slice(38, 42)}
+        </button>
+      ) : (
+        <button type="button" className="nav__connect" onClick={connectHandler}>
+          Connect
+        </button>
+      )}
+
+      <ul className="nav__links">
+        <li>
+          <a href="#Clothing & Jewellery">Clothing & Jewellery</a>
+        </li>
+        <li>
+          <a href="#Electronics and Gadgets">Electronics and Gadgets</a>
+        </li>
+        <li>
+          <a href="#Toys and Gaming">Toys and Gaming</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
 export default Navigation;
